25f257304b1333c3940f8495e1f37946b5feb912
Syver <syver.storm.furru@gmail.com>
2016-11-01 18:32:40 +0100

    Hello world server, readme.md added

---
+++ README.md
@@ -0,0 +1,5 @@
+=== Node REST API checklist ==
+
+1) Make git repository and initialize it / clone it from github/bitbucket etc.
+2) npm init, then install dependencies using npm install <DEP> --save
+3) node server.js (or other name) to start server
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => response.send('Hello world'));
+
+app.listen(port, () => {
+    console.log(`Example app listening port: ${port}`);
+});
